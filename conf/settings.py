82aeeec61732d4dc118f9e93b9705a259287d791
rogozin ivan <rogozin.ivan.1202@gmail.com>
2022-03-16 16:34:53 +0300

    Make postgresql as celery result backend

--- conf/settings.py
+++ conf/settings.py
@@ -193,7 +193,7 @@ CELERY_TIMEZONE = TIME_ZONE
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_BROKER_URL = os.getenv('REDIS_URL')
-CELERY_RESULT_BACKEND = os.getenv('REDIS_URL')
+CELERY_RESULT_BACKEND = 'db+postgresql://postgres:postgres@db:5432/postgres'
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 CELERY_BEAT_SCHEDULE = {
